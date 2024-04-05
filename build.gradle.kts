a1f3691193814b288ca931fd7ed1c44a4c642fce
Jovche Mitrejchevski <jovchemitrejchevski@Jovches-MacBook-Pro.local>
2024-04-05 12:30:21 +0200

    Update publication scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.jetbrainsKotlinSerialization) apply false
+    alias(libs.plugins.mavenPublish) apply false
 }
--- loctracksdk/build.gradle.kts
+++ loctracksdk/build.gradle.kts
@@ -1,16 +1,15 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.jetbrainsKotlinSerialization)
+    alias(libs.plugins.mavenPublish)
 }
 
 android {
     namespace = "io.github.mitrejcevski.locationtracker"
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
     compileSdk = libs.versions.compileSdkVersion.get().toInt()
 
     defaultConfig {
@@ -60,6 +59,40 @@ dependencies {
     testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
-apply {
-    from("../publish-package.gradle")
+mavenPublishing {
+    configure(
+        AndroidSingleVariantLibrary(
+            variant = "release",
+            sourcesJar = true,
+            publishJavadocJar = true
+        )
+    )
+    publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
+    signAllPublications()
+    coordinates("io.github.mitrejcevski", "locationtracker", "1.0.0-SNAPSHOT")
+    pom {
+        name.set("Location Tracker SDK")
+        description.set("This project is a helper SDK that helps tracing location.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/mitrejcevski/locationtracker")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("jovmit")
+                name.set("Jovche Mitrejchevski")
+                url.set("https://github.com/mitrejcevski/")
+            }
+        }
+        scm {
+            url.set("https://github.com/mitrejcevski/locationtracker/")
+            connection.set("scm:git:git://github.com/mitrejcevski/locationtracker.git")
+            developerConnection.set("scm:git:ssh://git@github.com/mitrejcevski/locationtracker.git")
+        }
+    }
 }
